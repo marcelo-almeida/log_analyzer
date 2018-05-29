713ef7550b3d91b1f467c77873d58419dee1cf8b
Marcelo <marcelo777_augusto@hotmail.com>
2018-05-28 22:53:18 -0300

    added generator of log in .txt

--- news.py
+++ news.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python3
 # This Python file uses the following encoding: utf-8
 
+import datetime
 from news_db import get_most_popular_articles, get_most_popular_authors, \
     get_failure_percentage
 
 
-# Using the news_db prints the answers for all questions
+# print result in console
 def print_question_and_answers():
-    print("What are the three most popular articles of all time?")
+    print(get_question_and_answers())
+
+
+# Using the news_db get the answers for all questions
+def get_question_and_answers():
+    result = "What are the three most popular articles of all time?\n"
     articles = get_most_popular_articles()
     for article in articles:
-        print(article)
-    print("Who are the authors of most popular articles of all time?")
+        result += article + "\n"
+    result += "Who are the authors of most popular articles of all time?\n"
     authors = get_most_popular_authors()
     for author in authors:
-        print(author)
-    print("what days the errors resulted more than 1% of requests?")
+        result += author + "\n"
+    result += "what days the errors resulted more than 1% of requests?\n"
     failures = get_failure_percentage()
     for failure in failures:
-        print(failure)
+        result += failure + "\n"
+    return result
+
+
+# save result in a file
+def save_question_and_answers():
+    output_file = open('answers' +
+                       datetime.datetime.now()
+                       .strftime('%Y-%m-%d-%H-%M-%S-%f')
+                       + '.txt', 'w')
+    output_file.write(get_question_and_answers())
+    output_file.close()
 
 
 print_question_and_answers()
+# save_question_and_answers()
