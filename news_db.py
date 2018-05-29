a1ddcf8400ae5322bf89e3db6f627716da662598
Marcelo <marcelo777_augusto@hotmail.com>
2018-05-28 21:05:49 -0300

    added fuction to get most popular authors

--- news_db.py
+++ news_db.py
@@ -3,10 +3,10 @@
 
 import psycopg2
 
-DBNAME = "news"
+DBNAME = 'news'
 
 most_popular_articles = '"{article}" - {count} views"'
-
+most_popular_authors = '{author} - {count} views'
 
 def execute_query(query):
     db = psycopg2.connect(database=DBNAME)
@@ -28,3 +28,22 @@ def get_most_popular_articles():
         )
         article_list.append(article)
     return article_list
+
+
+def get_most_popular_authors():
+    query = "SELECT authors.name, SUM(items.count) FROM " \
+            "(SELECT articles.author AS author_fk, count(log.path) AS count" \
+            " FROM log JOIN articles " \
+            "ON log.path = CONCAT('/article/', articles.slug) " \
+            "GROUP BY path, author) items JOIN authors " \
+            "ON items.author_fk = authors.id " \
+            "GROUP BY authors.id ORDER BY SUM(items.count) DESC"
+    query_results = execute_query(query)
+    author_list = []
+    for result in query_results:
+        author = most_popular_authors.format(
+            author=result[0],
+            count=str(result[1])
+        )
+        author_list.append(author)
+    return author_list
